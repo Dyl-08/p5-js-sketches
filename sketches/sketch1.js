b3209f3af34860d19ddfae90969115f1b39acbb9
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 09:24:03 -0400

    sketch1.js

--- sketches/sketch1.js
+++ sketches/sketch1.js
@@ -1,1 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
 
+function draw() {
+  background(32);
+  if(keyCode === LEFT_ARROW) {
+    fill(0, 0, 255);
+    circle(100, 100, 50)
+  } else if(keyCode === RIGHT_ARROW) {
+    fill(0, 255, 0);
+    circle(200, 200, 50);
+  } else if(keyCode === 32) {
+    fill(255);
+    circle(mouseX, mouseY, 75);
+  }
+}
