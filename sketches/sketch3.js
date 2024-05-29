8feae06525440baba03b5dceb54c01614b15e5ba
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 09:30:07 -0400

    sketch3.js

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,1 +1,16 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(32);
+  if(keyCode === 65) {
+    fill('purple');
+    triangle(100, 100, 150, 50, 200, 100);
+  } else if(keyCode === 66) {
+    fill('orange');
+    triangle(250, 250, 300, 200, 350, 250);
+  } 
+}
+
 
