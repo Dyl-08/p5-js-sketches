92c06a7e0a2480e311a68c1e031b82c155d710d6
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 10:01:28 -0400

    sketch5.js

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,1 +1,15 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  background(32); 
+}
+
+function draw() {
+}
+
+function mousePressed() {
+  for (let i = 0; i < 10; i++) {
+    fill(random(255), random(255), random(255));
+    rect(mouseX + i * 30, mouseY, 20, 20);
+  }
+}
 
