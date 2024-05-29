e3542f869d642135d4c7960255936ab8e1471372
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 09:55:03 -0400

    sketch4.js

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,1 +1,29 @@
+let mouseHeld = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  background(32); 
+}
+
+function draw() {
+}
+
+function mousePressed() {
+  if (!mouseHeld) {
+    background(32);
+    stroke(255);
+    line(0, 0, width, height);
+  }
+  mouseHeld = true; 
+}
+
+function mouseReleased() {
+  if (mouseHeld) {
+    background(32);
+    stroke(0, 0, 255);
+    line(width, 0, 0, height); 
+  }
+  mouseHeld = false; 
+}
+
 
