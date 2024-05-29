c7601e546fc4e132ea4af0454938a356d76e8465
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 10:55:03 -0400

    sketch3.js

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -13,4 +13,16 @@ function draw() {
   } 
 }
 
+function mousePressed() {
+  fill(255);
+  let side = 100;
+  let h = (sqrt(3) / 2) * side; // height of the equilateral triangle
+  let x1 = mouseX;
+  let y1 = mouseY - (2 / 3) * h;
+  let x2 = mouseX - side / 2;
+  let y2 = mouseY + (1 / 3) * h;
+  let x3 = mouseX + side / 2;
+  let y3 = mouseY + (1 / 3) * h;
+  triangle(x1, y1, x2, y2, x3, y3);
+}
 
