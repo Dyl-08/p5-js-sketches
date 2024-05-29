4ff6ab5de0307c512052383804281238e20a133d
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 09:25:06 -0400

    sketch1.js

--- sketches/sketch1.js
+++ sketches/sketch1.js
@@ -6,7 +6,7 @@ function draw() {
   background(32);
   if(keyCode === LEFT_ARROW) {
     fill(0, 0, 255);
-    circle(100, 100, 50)
+    circle(100, 100, 50);
   } else if(keyCode === RIGHT_ARROW) {
     fill(0, 255, 0);
     circle(200, 200, 50);
