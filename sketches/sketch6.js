07dc9acbeccef1cd752110ad3ef6c1039971b771
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 10:17:23 -0400

    sketch6.js

--- sketches/sketch6.js
+++ sketches/sketch6.js
@@ -1,1 +1,16 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  background(32); 
+}
+
+function draw() {
+}
+
+function mousePressed() {
+  for (let i = 0; i < 5; i++) {
+    stroke(random(255), random(255), random(255)); 
+    noFill();
+    circle(mouseX, mouseY, i * 50);
+  }
+}
 
