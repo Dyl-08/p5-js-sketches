f23c5b020da1e1bab8ab06bea4da771cba04c94e
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 09:52:12 -0400

    sketch2.js

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,1 +1,32 @@
+let keyHeld = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  background(32); 
+}
+
+function draw() {
+}
+
+function keyPressed() {
+  if (!keyHeld) {
+    fill(255, 255, 0); 
+    ellipse(width / 2, height / 2, 150, 100); 
+  }
+  keyHeld = true; 
+}
+
+function keyReleased() {
+  if (keyHeld) {
+    background(32);
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 75); 
+  }
+  keyHeld = false; 
+}
+
+function doubleClicked() {
+  clear(); 
+  background(32); 
+}
 
