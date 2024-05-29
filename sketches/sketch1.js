79b2d3f91a7cbc6714a8a2e17fa8d22ab777097a
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 14:31:35 -0400

    sketch1.js

--- sketches/sketch1.js
+++ sketches/sketch1.js
@@ -1,17 +1,17 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); // creating the Canvas with windowWidth and windowHeight
 }
 
 function draw() {
   background(32);
-  if(keyCode === LEFT_ARROW) {
-    fill(0, 0, 255);
-    circle(100, 100, 50);
-  } else if(keyCode === RIGHT_ARROW) {
-    fill(0, 255, 0);
-    circle(200, 200, 50);
-  } else if(keyCode === 32) {
-    fill(255);
-    circle(mouseX, mouseY, 75);
+  if(keyCode === LEFT_ARROW) { // keyCode for left arrow
+    fill(0, 0, 255); // fill blue
+    circle(100, 100, 50); // (coordinates and diameter)
+  } else if(keyCode === RIGHT_ARROW) { // else if operaator
+    fill(0, 255, 0); // fill green
+    circle(200, 200, 50); // (coordinates and diameter)
+  } else if(keyCode === 32) { // keycode 32 is spacebar
+    fill(255); // fill white
+    circle(mouseX, mouseY, 75); // circle appears at mouse coordinates with a diameter of 75
   }
 }
