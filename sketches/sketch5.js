fe52de2c92dcf65b00251525f91368c396df7f20
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 10:06:00 -0400

    sketch5.js

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -7,7 +7,7 @@ function draw() {
 }
 
 function mousePressed() {
-  for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < 10; i++) {
     fill(random(255), random(255), random(255));
     rect(mouseX + i * 30, mouseY, 20, 20);
   }
