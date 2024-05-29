58176d62d72498f55f8b560783b4ac74f4b3c1d3
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 15:01:51 -0400

    sketch5.js

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -6,10 +6,10 @@ function setup() {
 function draw() {
 }
 
-function mousePressed() {
+function mousePressed() { // if the mouse is pressed 10 circles of randomly different colours will iterate through the for loop through the variable i. 
   for (let i = 0; i < 10; i++) {
     fill(random(255), random(255), random(255));
-    rect(mouseX + i * 30, mouseY, 20, 20);
+    rect(mouseX + i * 30, mouseY, 20, 20); // The circles are separated by 30 units.
   }
 }
 
