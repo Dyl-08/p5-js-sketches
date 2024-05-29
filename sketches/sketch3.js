7df4290f7f2e76beba969893f73b26225059805f
Dyl-08 <149424000+Dyl-08@users.noreply.github.com>
2024-05-29 15:00:07 -0400

    sketch3.js

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,28 +1,20 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); // canvas size set to windowWidth and windowHeight 
 }
 
 function draw() {
   background(32);
-  if(keyCode === 65) {
-    fill('purple');
-    triangle(100, 100, 150, 50, 200, 100);
-  } else if(keyCode === 66) {
-    fill('orange');
-    triangle(250, 250, 300, 200, 350, 250);
+  if(keyCode === 65) { // if 'A' key is pressed
+    fill('purple'); // set the fill colour to purple
+    triangle(100, 100, 150, 50, 200, 100); // triangle coordinates
+  } else if(keyCode === 66) { // else if 'B' key is pressed
+    fill('orange'); // set the fill colour to orange 
+    triangle(250, 250, 300, 200, 350, 250); // tirnalge coordinates
   } 
 }
 
-function mousePressed() {
+function mousePressed() { // ensures that if the mouseIsPressed a equilateral triangle of side length 100 is centered at the mouse coordinates
   fill(255);
-  let side = 100;
-  let h = (sqrt(3) / 2) * side; // height of the equilateral triangle
-  let x1 = mouseX;
-  let y1 = mouseY - (2 / 3) * h;
-  let x2 = mouseX - side / 2;
-  let y2 = mouseY + (1 / 3) * h;
-  let x3 = mouseX + side / 2;
-  let y3 = mouseY + (1 / 3) * h;
-  triangle(x1, y1, x2, y2, x3, y3);
+  triangle(mouseX, mouseY, mouseX + 50, mouseY - 50, mouseX + 100, mouseY)
 }
 
